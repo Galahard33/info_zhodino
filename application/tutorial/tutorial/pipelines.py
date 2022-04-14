0e881b391270d91791ed900d615777c70dcc0ece
Galahard33 <82763387+Galahard33@users.noreply.github.com>
2022-04-14 18:42:42 +0300

    Update pipelines.py

--- application/tutorial/tutorial/pipelines.py
+++ application/tutorial/tutorial/pipelines.py
@@ -4,10 +4,10 @@ from itemadapter import ItemAdapter
 
 class ImageCinemaPipeline(object):
     def open_spider(self, spider):
-        hostname = 'ec2-3-223-213-207.compute-1.amazonaws.com'
-        username = 'grwusbywcvffav'
-        password = 'f78c737b40f3de26588eb2dc63ee0b83cfa9d4a17c341cba0fe2509d2136b5e7'  # your password
-        database = 'd2579bis4gp1rt'
+        hostname = ''
+        username = ''
+        password = ''  # your password
+        database = ''
         self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
         self.cur = self.connection.cursor()
         self.cur.execute("DELETE FROM test")
